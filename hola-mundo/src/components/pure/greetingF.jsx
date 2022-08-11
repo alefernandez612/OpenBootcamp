aaf4a7ec2648b2fd4c1610976b4f452951a0b816
alefernandez612 <ale.fernandez612@gmail.com>
2022-08-11 18:19:32 +0200

    Session 2

---
+++ hola-mundo/src/components/pure/greetingF.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+
+const GreetingF = (props) => {
+
+    const [age, setAge] = useState(29);
+
+    const birthday = () => {
+        setAge(age + 1);
+    };
+
+    return (
+        <div>
+            <h1>HOLA {props.name}!!</h1>
+            <h2>Tu edad es de: {age}</h2>
+            <div>
+                <button onClick={birthday}>
+                    Add age
+                </button>
+            </div>
+        </div>
+    );
+};
+
+
+GreetingF.propTypes = {
+    name: PropTypes.string
+};
+
+
+export default GreetingF;
