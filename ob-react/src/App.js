e92b0f3fa1e9f2b6d28e44084fb067ea7bbb543e
alefernandez612 <ale.fernandez612@gmail.com>
2022-08-13 14:06:13 +0200

    Finish Ejemplo4

--- ob-react/src/App.js
+++ ob-react/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import MiComponenteConContexto from "./hooks/Ejemplo3";
+import Ejemplo4 from './hooks/Ejemplo4';
+// import MiComponenteConContexto from "./hooks/Ejemplo3";
 // import Ejemplo2 from './hooks/Ejemplo2';
 // import Ejemplo1 from './hooks/Ejemplo1';
 
@@ -11,7 +12,12 @@ function App() {
         <img src={ logo } className="App-logo" alt="logo" />
         {/* <Ejemplo1></Ejemplo1> */ }
         {/* <Ejemplo2></Ejemplo2> */ }
-        <MiComponenteConContexto></MiComponenteConContexto>
+        {/* <MiComponenteConContexto></MiComponenteConContexto> */ }
+        <Ejemplo4 nombre='Alejandro'>
+          <h3>
+            Props.children
+          </h3>
+        </Ejemplo4>
       </header>
     </div>
   );
---
+++ ob-react/src/hooks/Ejemplo4.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Ejemplo4 = (props) => {
+    return (
+        <div>
+            <h1>Ejemplo de children props</h1>
+            <h2>
+                { props.nombre }
+            </h2>
+            { props.children }
+        </div>
+    );
+};
+
+
+export default Ejemplo4;
